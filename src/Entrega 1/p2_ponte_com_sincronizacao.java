6e99d7f72550d0a96e238d0bf96368753a2f7a43
Luana de Melo Queiroz <136331050+luana-queiroz@users.noreply.github.com>
2024-06-18 15:07:34 -0300

    Create p2_ponte_com_sincronizacao.java
    problema 2 com sincronizacao

---
+++ src/Entrega 1/p2_ponte_com_sincronizacao.java
@@ -0,0 +1,49 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class p2_ponte_com_sincronizacao {
+
+    public static class Ponte{
+        private boolean ponteLivre = true;
+        private final Lock lock = new ReentrantLock();
+
+        public void atravessarPonte(String sentido) throws InterruptedException {
+            System.out.println("Vem aí um carro (" + Thread.currentThread().getName()+ ") da " + sentido + " querendo atravessar.");
+            lock.lock();
+            try { 
+                this.ponteLivre = false; 
+                System.out.println("Tem um carro (" + Thread.currentThread().getName()+ ") atravessando a ponte.");
+            } finally{
+                Thread.sleep(2000);
+                System.out.println("O carro atravessou a ponte com sucesso!");
+                this.ponteLivre = true;
+                lock.unlock();
+            }
+        }
+    }
+  
+    public static class Carro implements Runnable{
+        private final String sentido; //sentido diz se vem da direita ou esquerda
+        private final Ponte ponte;
+
+        public Carro(String sentido, Ponte ponte){
+            this.sentido = sentido;
+            this.ponte = ponte;
+        }
+
+        public void run() {
+            try {
+                this.ponte.atravessarPonte(this.sentido);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Ponte ponte = new Ponte();
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Carro(i % 2 == 0 ? "esquerda":"direita", ponte)).start();
+        }
+    }
+}
