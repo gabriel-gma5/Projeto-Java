020b369180c06d97bc10bb442621cc289e0b7d29
Luana de Melo Queiroz <136331050+luana-queiroz@users.noreply.github.com>
2024-06-18 14:53:32 -0300

    Update and rename p2_ponte.java to p2_ponte_nao_sincronizada.java
    Problema 2 - ponte não sincronizada

--- src/Entrega 1/p2_ponte.java
+++
@@ -1,7 +0,0 @@
-
-public class p2_ponte {
-    
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ src/Entrega 1/p2_ponte_nao_sincronizada.java
@@ -0,0 +1,29 @@
+public class p2_ponte_nao_sincronizada {
+    public static class Carro implements Runnable{
+        private final String sentido; //sentido vai dizer se vai para direita ou esquerda
+
+        public Carro(String sentido){
+            this.sentido = sentido;
+        }
+
+        public void run() {
+            try {
+                atravessar();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void atravessar() throws InterruptedException {
+            System.out.println("Vem aí um carro (" + Thread.currentThread().getName()+ ") da " + sentido + ".");
+            Thread.sleep(1000);
+            System.out.println("O carro (" + Thread.currentThread().getName()+ ") atravessou a ponte com sucesso!");
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Carro(i % 2 == 0 ? "esquerda":"direita")).start();
+        }
+    }
+}
