90de1fb4c3b400131e4a0b6ff801f123042fa729
gabriel-gma5 <gma5@cin.ufpe.br>
2024-06-21 20:08:51 -0300

    p2_wo_sync explaining synchronization issue

--- src/Entrega1/p2_ponte_nao_sincronizada.java
+++ src/Entrega1/p2_ponte_nao_sincronizada.java
@@ -1,32 +1,59 @@
-public class p2_ponte_nao_sincronizada {
-    public static class Carro implements Runnable{
-        private final String sentido; //sentido diz se veio da direita ou esquerda
 
-        public Carro(String sentido){
+public class p2_ponte_nao_sincronizada {
+                
+    public static class Bridge {
+        public int left;
+        public int right;
+    }
+
+    public static class Carro extends Thread{
+        private final String sentido; //sentido diz se veio da direita ou esquerda
+        private final Bridge ponte;
+        public Carro(String sentido, Bridge ponte){
             this.sentido = sentido;
+            this.ponte = ponte;
         }
 
         public void run() {
             try {
-                atravessar();
+                atravessar(ponte);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
 
-        private void atravessar() throws InterruptedException {
+        private void atravessar(Bridge ponte) throws InterruptedException {
             System.out.println("Vem aí um carro (" + Thread.currentThread().getName()+ ") da " + sentido + ".");
+            
+            if(sentido == "esquerda") {ponte.left +=1;}
+            else {ponte.right += 1;}
+            Thread.sleep(0,100000);
+
+            int otherSideStatus = (sentido == "esquerda")? ponte.right : ponte.left;
+        
             Thread.sleep(1000);
-            System.out.println("O carro (" + Thread.currentThread().getName()+ ") atravessou a ponte com sucesso!");
+            if(otherSideStatus<1){
+                System.out.println("O carro (" + Thread.currentThread().getName()+ ") atravessou a ponte com sucesso!");
+            }
+            else{
+                System.out.println(Thread.currentThread().getName()+" tentou atravessar da "+sentido+" mas tinha "+otherSideStatus+" carros atrapalhando!");
+            }
+            if(sentido == "esquerda") {ponte.left -=1;}
+            else {ponte.right -= 1;}
         }
     }
 
+
     public static void main(String[] args) {
+        Bridge ponte = new Bridge();
+        Thread carro;
         for (int i = 0; i < 10; i++) {
-            new Thread(new Carro(i % 2 == 0 ? "esquerda":"direita")).start();
+            carro = new Carro(i % 2 == 0 ? "esquerda":"direita", ponte);
+            carro.setName("Carro-"+i);
+            carro.start();
         }
-        // os carros não estão sincronizados, então todos "atravessam ao mesmo tempo".
-        // isto é, depois de 1000ms é mostrado que todos eles passaram, o que não deveria ser possível.
-        // o comportamento esperado era o de um carro atravessar por vez.
+        // os carros, por não estarem sincronizados, não conseguem atravessar a ponte um de cada vez.
+        // então, ao tentar atravessar a ponte sem garantir a sua vez, ocorre um deadlock
+        // em que nenhum deles libera o espaço (subtrai contador do seu lado) para o próximo passar
     }
 }
