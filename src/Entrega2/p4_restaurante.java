6d463c4428ce1ab2e75a54c43a4a8aba6fc01c53
gabriel-gma5 <gma5@cin.ufpe.br>
2024-07-04 22:30:17 -0300

    p4 almost finished (needs revision)

--- src/Entrega2/p4_restaurante.java
+++ src/Entrega2/p4_restaurante.java
@@ -7,11 +7,10 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /*
  * --- Criar metodo para ficar tentando adquirir uma cadeira ---
- *  - Precisa considerar quando estiverem cheias (0 disponiveis):
-*   - Se cheia: espera ter 5 disponiveis (fila - while1 com reg.crit?)    
-*   - Caso contrario:
-*     - cheque se o status "recentemente lotado" esta ativo:
-*       - Nao ativo (5 clientes juntos ja terminaram de jantar):
+ *  - Precisa considerar quando estiverem cheias (0 disponiveis)
+ *  - Precisa considerar a fila, o primeiro dela e se ja esta na fila 
+*     - Cheque se o status "recentemente lotado" esta ativo:
+*       - Nao ativo (5 clientes juntos ja terminaram de jantar ou ainda nao tiveram 5 clientes que jantaram juntos):
 *           Tente adquirir uma cadeira disponivel (while 2 com reg.crit?)
 *       - Ativo (5 clientes acabaram de comecar a jantar juntos (available=5) ou /
 *               ainda tem clientes de um grupo de 5 que estao terminando de jantar (available<5)):
@@ -24,41 +23,62 @@ import java.util.concurrent.locks.ReentrantLock;
 */
 
 class Restaurant {
-    public static Semaphore dinningSemaphore = new Semaphore(5, true);
     public static int availableChairs = 5;
-    private static final int total = 5;
+    public static Semaphore dinningSemaphore = new Semaphore(availableChairs, true);
     public static boolean recentlyFull = false;
+    private static final int totalChairs = 5;
     private static final Lock mutex = new ReentrantLock();
-    public static void tryGetChair(Customer customer){
+    public static int servedCust = 0;
+    public static int totalCust;     
+    
+    public static boolean tryGetChair(Customer customer){
         try {
             mutex.lock();
             if (!Restaurant.recentlyFull) {
                 System.out.println(Thread.currentThread().getName()+" conseguiu uma cadeira e agora ira jantar");
-                availableChairs+=1;
-                if(availableChairs == total){recentlyFull = true;}
-            else{
-                WaitingQueue.insert(customer);
-                System.out.println(Thread.currentThread().getName()+" nao conseguiu uma cadeira e vai ter que ir para a fila");
+                availableChairs-=1;
+                if(availableChairs == totalChairs){recentlyFull = true;}
+                // se esta na fila, entao sai
+                if(customer.prev!=null | customer == WaitingQueue.tail){WaitingQueue.leaveQ();}
+                return true;
+            }else{
+                // se nao esta na fila, entao entra
+                if(customer.prev == null & customer!=WaitingQueue.tail){WaitingQueue.insert(customer);}
+                System.out.println(Thread.currentThread().getName()+" nao conseguiu uma cadeira e vai ter que esperar na fila");
+                return false;    
                 }
             }
-            else{
-            }
-        }
         finally{
             mutex.unlock();
             }
-        }
+    }
 
     public static void dinner(Customer customer){
-
+        try {
+            dinningSemaphore.acquire();
+            System.out.println(Thread.currentThread().getName()+" Esta jantando");
+            Thread.sleep(100);
+            System.out.println(Thread.currentThread().getName()+" terminou de jantar");
+            mutex.lock();
+                availableChairs+=1;
+                if(availableChairs == 0){recentlyFull = false;}
+                servedCust+=1;
+            mutex.unlock();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        finally{
+            dinningSemaphore.release();
+        }
     }
 }
 
 class WaitingQueue extends Thread {
-    private static Customer head = null, tail = null;
+    public static Customer head = null, tail = null;
+    
     public static Customer leaveQ(){
         Customer first = head;
-        head = head.prev;
+        head = head.prev; 
         return first;
     }
     public static void insert(Customer newC){
@@ -69,30 +89,45 @@ class WaitingQueue extends Thread {
             tail.prev = newC;
             tail = newC;}
     }
+
     @Override
     public void run(){
-        head.run();
-        
+        while(Restaurant.servedCust<Restaurant.totalCust){
+            while(head!=null){
+                head.run();
+            }
+        }
     }
 } 
 
 class Customer extends Thread {
-    public Customer prev = null, next = null;
-    private Restaurant rest_;
-    private WaitingQueue queue;
-    public Customer(Restaurant Rest_, WaitingQueue Queue){
-        this.rest_ = Rest_;
-        this.queue = Queue;
-    }
+    public Customer prev = null;
     @Override
     public void run(){
-        Restaurant.tryGetChair(this);
+        if(Restaurant.tryGetChair(this)){
+            Restaurant.dinner(this);
+        };
     }
 }
 
 
 public class p4_restaurante {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Restaurant.totalCust = 20;
+        WaitingQueue queue = new WaitingQueue();
+        Customer[] customers = new Customer[Restaurant.totalCust];
+        for (int i = 0; i < Restaurant.totalCust; i++) {
+            customers[i] = new Customer();
+            customers[i].setName("Cliente-"+(i+1));
+        }
         
+        queue.start();
+        for (Customer customer : customers) {
+            customer.start();
+        }    
+       
+        queue.join(); 
+        System.out.println("Todos os clientes foram atendidos.");
+       
     }
 }
