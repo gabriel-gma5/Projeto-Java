90bbe92cd62b57f9e2864494111e8bde5ad6588a
Luana de Melo Queiroz <136331050+luana-queiroz@users.noreply.github.com>
2024-07-26 20:59:40 -0300

    Update p5_transporte.java
    Ajustes finais da q5

--- src/Entrega3/p5_transporte.java
+++ src/Entrega3/p5_transporte.java
@@ -11,16 +11,18 @@ class paradaOnibus {
     private int pessoasParada = 0; //quantidade de pessoas na parada
     private int passageirosOnibus = 0;
     private Lock lock = new ReentrantLock();
-    private Condition onibusChegou = lock.newCondition();
     private Semaphore capacidadeOnibus = new Semaphore(limitePessoas, true); //pra gerenciar quantos passageiros podem entrar
+    private Condition onibusChegada = lock.newCondition();
+    private Condition onibusPartida = lock.newCondition();
+    public int pessoasEmbarque = 0;
 
     //passageiro chega na parada
     public void chegouParada() throws InterruptedException {
         lock.lock();
         try {
             pessoasParada++;
-            System.out.println("Agora tem " + pessoasParada + " pessoas na parada esperando.");
-            onibusChegou.await(); //espera o onibus chegar
+            System.out.println(Thread.currentThread().getName() + " (Passageiro) chegou na parada. Agora tem " + pessoasParada + " pessoas na parada esperando pelo ônibus.");
+            onibusChegada.await(); //espera o onibus chegar
         } finally {
             lock.unlock();
         }
@@ -28,40 +30,45 @@ class paradaOnibus {
 
     //passageiro tenta entrar no onibus
     public void entrarPassageiro() throws InterruptedException {
-        capacidadeOnibus.acquire();
         lock.lock();
+        //ao tentar entrar, há a chance de não ter mais cadeiras; necessário esperar o próximo ônibus
+        while (!capacidadeOnibus.tryAcquire())
+        {onibusChegada.await();}
+
         try {
             pessoasParada--;
-            System.out.println("Um passageiro acabou de entrar no ônibus.");
+            System.out.println("Um passageiro saiu da parada e acabou de entrar no ônibus |  pessoas na parada: "+pessoasParada);
             passageirosOnibus++;
-            if (pessoasParada == 0 ){
-                onibusChegou.signalAll(); //indica que caso não tenha mais ninguem na parada, é pro onibus partir
+            pessoasEmbarque--;
+            if (pessoasEmbarque == 0 || passageirosOnibus == limitePessoas){ //caso não tenha mais ninguem na parada ou lotem os assentos, é pro onibus partir
                 System.out.println("O ônibus recebeu ordem de partir, e vai sair com " + passageirosOnibus + " passageiros.");
-            }else if (passageirosOnibus == limitePessoas){
-                System.out.println("O ônibus recebeu ordem de partir, e vai sair com " + passageirosOnibus + " passageiros.");
+                onibusPartida.signal();
             }
         } finally {
+            Thread.sleep(new Random().nextInt(15));
             lock.unlock();
         }
     }
 
     //onibus chega na parada
-    public void onibusChegou() throws InterruptedException {
+    public void onibusChegada() throws InterruptedException {
         lock.lock();
         try {
             System.out.println("Um ônibus acabou de chegar na parada, que tem " + pessoasParada + " passageiros esperando.");
-            onibusChegou.signalAll();
+            pessoasEmbarque = pessoasParada;
+            onibusChegada.signalAll();
+            System.out.println("Onibus esperando os passageiros subirem");
+            onibusPartida.await();
+
+            System.out.println("Ônibus partiu com " + passageirosOnibus + " pessoas nos assentos");
+
+            capacidadeOnibus.release(passageirosOnibus);
+            passageirosOnibus = 0;
         } finally {
             lock.unlock();
         }
 
-        //tempo que o onibus fica parado na parada
-        //eita acho que tenho que ajeitar isso
-        Thread.sleep(1000 + new Random().nextInt(2000));
 
-        passageirosOnibus = 0;
-
-        capacidadeOnibus.release(limitePessoas);
     }
 
     //quantidade de passageiros esperando na parada
@@ -105,11 +112,14 @@ class Onibus extends Thread {
         while (true) {
             try {
                 if (parada.passageirosEsperando() > 0) {
-                    parada.onibusChegou();
+                    parada.onibusChegada();
                 } else {
+                    System.out.println("Ônibus partindo. Sem pessoas na parada");
                     //quando o onibus chega e nao tem ninguem na parada
-                    Thread.sleep(1000 + new Random().nextInt(2001));
                 }
+                int val = 1000 + new Random().nextInt(2001);
+                Thread.sleep(val);
+                System.out.println("Outro ônibus chegou em " + val + " milissegundos");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -117,22 +127,21 @@ class Onibus extends Thread {
     }
 }
 
-public class p5_transporte {
+public class p5v2_transporte {
     public static void main(String[] args) {
         paradaOnibus parada = new paradaOnibus();
 
         Onibus bus = new Onibus(parada);
         bus.start();
 
-        //100 passageiros chegando
-        for (int i = 0; i < 100; i++) {
-            System.out.println("Chegou mais um passageiro.");
+        //passageiros chegando
+        for (int i = 0; i < 200; i++) {
             Passageiro passenger = new Passageiro(parada);
             passenger.start();
 
             //tempo entre passageiros
             try {
-                Thread.sleep(new Random().nextInt(500));
+                Thread.sleep(new Random().nextInt(100)+5);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
